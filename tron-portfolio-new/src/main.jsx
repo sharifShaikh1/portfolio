866360be8fe59a12e65a378fcd9e4f9beef5c60b
Sharif <sharif3534@gmail.com>
2025-05-17 00:44:43 +0530

    Update PortFolio.jsx with project links, update Tailwind and PostCSS configs, and modify main.jsx

--- tron-portfolio-new/src/main.jsx
+++ tron-portfolio-new/src/main.jsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-
 import './index.css';
-import Portfolio from './components/PortFolio.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-  
-    <Portfolio/>
+    <App />
   </React.StrictMode>
 );
